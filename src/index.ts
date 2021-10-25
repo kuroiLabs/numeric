d4e2c08a0d66fbb21600a352ead390ab74b07327
Jesse Rogers <jesse@jesserogers.net>
2021-10-24 23:47:26 -0700

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as Typing from './typing'
 import * as Generate from './generate'
 
 export namespace Numeric {
-  Bitwise
-  Generate
-  Typing
+  export const bitwiset = Bitwise
+  export const generate = Generate
+  export const typing = Typing
 }
