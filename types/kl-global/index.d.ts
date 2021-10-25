559244c33bee8fe1f7ed2fabab498ac15e1b2b97
Jesse Rogers <jesse@jesserogers.net>
2021-10-24 22:40:25 -0700

    add types

---
+++ types/kl-global/index.d.ts
@@ -0,0 +1,11 @@
+// numeric types
+type bit = 0 | 1 | boolean
+type byte = number
+type uint16 = number
+type int16 = number
+type uint32 = number
+type int32 = number
+type uint64 = number
+type int64 = number
+type float = number
+type int = number
